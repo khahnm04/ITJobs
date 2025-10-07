c33a1230b6d0d622f10850e4879a83a3d2336a52
khahnm04 <khanhnguyenminh1811@gmail.com>
2025-10-07 18:27:49 +0700

    Dựng Footer

---
+++ src/app/components/footer/Footer.tsx
@@ -0,0 +1,16 @@
+export const Footer = () => {
+  return (
+    <>
+      <div className="bg-[#000065] py-[24px]">
+        <div className="container">
+          <div className="font-[800] text-[28px] text-white text-center mb-[10px]">
+            28.ITJobs
+          </div>
+          <div className="font-[400] text-[14px] text-[#A6A6A6] text-center">
+            Copyright © IT VIEC JSC
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { Header } from "./components/header/Header";
+import { Footer } from "./components/footer/Footer";
 
 export const metadata: Metadata = {
   title: "Dự án 3",
@@ -18,6 +19,8 @@ export default function RootLayout({
         <Header />
 
         {children}
+
+        <Footer />
       </body>
     </html>
   );
