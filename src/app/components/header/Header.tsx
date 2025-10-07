d08ce52299f335fa8e6b7438cc77d8d8f0cf1e21
khahnm04 <khanhnguyenminh1811@gmail.com>
2025-10-07 14:09:35 +0700

    Dựng Header: Responsive giao diện

--- src/app/components/header/Header.tsx
+++ src/app/components/header/Header.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link"
-import { FaAngleDown, FaAngleRight } from "react-icons/fa6";
+import { FaAngleDown, FaAngleRight, FaBars } from "react-icons/fa6";
 
 export const Header = () => {
   return (
@@ -8,11 +8,11 @@ export const Header = () => {
         <div className="container">
           <div className="flex justify-between items-center">
             {/* Logo */}
-            <Link href="#" className="font-[800] text-[28px] text-white">
+            <Link href="#" className="font-[800] sm:text-[28px] text-[20px] text-white lg:flex-none flex-1">
               28.ITJobs
             </Link>
             {/* Menu */}
-            <nav className="">
+            <nav className="lg:block hidden">
               <ul className="flex gap-x-[30px]">
                 <li className="inline-flex items-center gap-x-[8px] relative group/sub-1">
                   <Link href="#" className="font-[600] text-[16px] text-white">
@@ -61,15 +61,19 @@ export const Header = () => {
               </ul>
             </nav>
             {/* Account */}
-            <div className="font-[600] text-[16px] text-white inline-flex gap-x-[5px]">
+            <div className="font-[600] sm:text-[16px] text-[12px] text-white inline-flex gap-x-[5px]">
               <Link href="#" className="">
                 Đăng Nhập
               </Link>
               <span className="">/</span>
               <Link href="#" className="">
-                Đăng Nhập
+                Đăng Ký
               </Link>
             </div>
+            {/* Button Menu Mobile */}
+            <button className="lg:hidden ml-[12px]">
+              <FaBars className="text-[20px] text-white" />
+            </button>
           </div>
         </div>
       </header>
